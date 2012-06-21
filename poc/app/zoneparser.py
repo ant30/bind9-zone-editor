0f602c10c107a90f8583a4df31f0c006e51ee433
ant30 <ant30tx@gmail.com>
2012-06-22 00:53:17 +0200

    Add a new parser for file, its simpler than easydns

---
+++ poc/app/zoneparser.py
@@ -0,0 +1,21 @@
+import re
+
+
+
+# SERIAL = yyyymmddnn ; serial
+EXPRESIONS = {
+    'serial':re.compile(r'(?P<serial>\d{10}) *; *serial'),
+    'record':re.compile(r'^(?P<name>(?:[a-zA-Z0-9-.]*|@)) *(?P<recordtype>A|CNAME) *(?P<target>[a-zA-Z0-9-.]*)(?: *;(?P<comment>.*)$|)')
+}
+
+zonefile = open('db.sevi.mobi')
+for line in zonefile.readlines():
+    serial_line = EXPRESIONS['serial'].search(line)
+    if serial_line:
+        print "SERIAL:", serial_line.group('serial')
+        continue
+    record_line = EXPRESIONS['record'].search(line)
+    if record_line:
+        print "RECORD:", record_line.groups()
+
+
