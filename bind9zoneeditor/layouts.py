80f44df7c0a6f68642c4a29896b354cc6dd9de68
ant30 <ant30tx@gmail.com>
2012-06-26 18:49:10 +0200

    Fix DOCTYPE

--- bind9zoneeditor/layouts.py
+++ bind9zoneeditor/layouts.py
@@ -6,7 +6,7 @@ class Layouts(object):
         @reify
         def global_template(self):
             renderer = get_renderer("templates/global_layout.pt")
-            return renderer.implementation().macros['layout']
+            return renderer.implementation()
 
         @reify
         def global_macros(self):
